1de243bd4eb75cb4a3ba4fc04ebb5999b47aa71b
Mohadese Akhoondy <M.akhoondy1381@gmail.come>
2023-01-02 12:19:38 +0330

    modified first 3 classes

--- IMDBTeamWork/IMDBTeamWork/FavoriteList.cs
+++ IMDBTeamWork/IMDBTeamWork/FavoriteList.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace IMDBTeamWork
 {
-    internal class FavoriteList
+    public class FavoriteList
     {
+        private string favoriteListName;
+        public Movie[] favoriteList = new Movie[100];
+        private User creatorUserOfFavoriteList;
+
+        public FavoriteList(string favoriteListName, User user)
+        {
+            this.favoriteListName = favoriteListName;
+            creatorUserOfFavoriteList = user;
+        }
+        public string FavoriteListName { get { return favoriteListName; } set { favoriteListName = value; } }
     }
 }
--- IMDBTeamWork/IMDBTeamWork/Movie.cs
+++ IMDBTeamWork/IMDBTeamWork/Movie.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace IMDBTeamWork
 {
-    internal class Movie
+    public class Movie
     {
+        private string tconst;
+        private string titleType;
+        private string primaryTitle;
+        private string originalTitle;
+        private int isAdult;//0 = false 1= true
+        private string startYear;
+        private string endYear;
+        private string runtimeMinutes;
+        private string[] genres = new String[3];//includes up to three genres
+
+        public Movie(string tconst, string titleType, string primaryTitle, string originalTitle, int isAdult, string startYear, string endYear, string runtimeMinutes)
+        {
+            this.tconst = tconst;
+            this.titleType = titleType;
+            this.primaryTitle = primaryTitle;
+            this.originalTitle = originalTitle;
+            this.isAdult = isAdult;
+            this.startYear = startYear;
+            this.endYear = endYear;
+            this.runtimeMinutes = runtimeMinutes;
+        }
     }
 }
