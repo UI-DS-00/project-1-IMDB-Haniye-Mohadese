e46bb59fdffe8fb4d9db8d8e7a7dcb4b7270bcef
Mohadese Akhoondy <M.akhoondy1381@gmail.come>
2023-01-03 14:19:31 +0330

    add changes

--- IMDB/IMDB/Person.cs
+++ IMDB/IMDB/Person.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace IMDB
 {
-    internal class Person
+    public class Person
     {
+        public string NConst { get; set; }
+        public string PrimaryName { get; set; }
+        public string BirthYear { get; set; }
+        public string DeathYear { get; set; }
+        public string PrimaryProfession { get; set; }
+        public string KnownForTitles { get; set; }
+        public string linkImage { get; set; }
+
+        public Person(string nConst, string primaryName, string birthYear, string deathYear,
+            string primaryProfession, string knownForTitles, string linkImage)
+        {
+            NConst = nConst;
+            PrimaryName = primaryName;
+            BirthYear = birthYear;
+            DeathYear = deathYear;
+            PrimaryProfession = primaryProfession;
+            KnownForTitles = knownForTitles;
+            this.linkImage = linkImage;
+        }
     }
 }
